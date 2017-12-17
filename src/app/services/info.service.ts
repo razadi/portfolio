d254c0f7f62f0cdbdf66c2c4cd2c7eccc6c49062
Raúl Zamudio <razadi07@gmail.com>
2017-12-16 21:24:57 -0600

    Servicio de firebase ok

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -6,15 +6,31 @@ export class InfoService {
   
   info: any = {};
   cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
+  equipo: any[] = [];
 
   constructor(public http: Http) {
     
+    this.carga_info();
+    this.carga_sobre_nosotros();
+    
+   }
+
+  public carga_info(){
     this.http.get("assets/data/info.page.json")
-    .subscribe( data => {
-      console.log(data.json());
-      this.cargada = true;
-      this.info = data.json();
+      .subscribe(data => {
+        // console.log(data.json());
+        this.cargada = true;
+        this.info = data.json();
       });
+  }
+   public carga_sobre_nosotros(){
+     this.http.get("https://portafolio-aef53.firebaseio.com/equipo.json")
+       .subscribe(data => {
+         // console.log(data.json());
+         this.cargada_sobre_nosotros = true;
+         this.equipo = data.json();
+       });
    }
 
 }
