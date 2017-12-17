4c8e502d022b5c0482c1a554d221049fd674b642
Raúl Zamudio <razadi07@gmail.com>
2017-12-16 20:02:43 -0600

    Routing creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas
+import { app_routing } from "./app.routes";
 
+// componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './component/home/home.component';
 import { AboutComponent } from './component/about/about.component';
 import { ItemComponent } from './component/item/item.component';
+import { RouterModule } from '@angular/router/src/router_module';
 
 
 @NgModule({
@@ -20,7 +24,8 @@ import { ItemComponent } from './component/item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
